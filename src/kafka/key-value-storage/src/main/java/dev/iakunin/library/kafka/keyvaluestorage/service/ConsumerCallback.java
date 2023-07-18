49906bff831db1db63a6bfe70b8d5b79a04e8cde
Maksim Iakunin <yakuninm+github@gmail.com>
2023-07-18 16:12:41 +0300

    2023-07-18 Adding :kafka:key-value-storage

---
+++ src/kafka/key-value-storage/src/main/java/dev/iakunin/library/kafka/keyvaluestorage/service/ConsumerCallback.java
@@ -0,0 +1,53 @@
+package dev.iakunin.library.kafka.keyvaluestorage.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.connect.util.Callback;
+
+@Slf4j
+@RequiredArgsConstructor
+@SuppressWarnings("checkstyle:WhitespaceAround")
+public class ConsumerCallback<K, V> implements Callback<ConsumerRecord<String, byte[]>> {
+
+    private final ObjectMapper objectMapper;
+    private final Map<K, V> map;
+
+    @Override
+    public void onCompletion(Throwable error, ConsumerRecord<String, byte[]> record) {
+        if (error != null) {
+            log.error("Unexpected in consumer callback: ", error);
+            return;
+        }
+
+        final K deserializedKey;
+        try {
+            deserializedKey = objectMapper.readValue(record.key(), new TypeReference<>() {});
+        } catch (JsonProcessingException ex) {
+            log.error("Failed to deserialize key: ", ex);
+            return;
+        }
+
+        final V deserializedValue;
+        try {
+            deserializedValue =
+                objectMapper.readValue(record.value(), new TypeReference<>() {});
+        } catch (IOException ex) {
+            log.error("Failed to deserialize value: ", ex);
+            return;
+        }
+
+        if (record.value() == null) {
+            log.info("Removing by key='{}'", deserializedKey);
+            map.remove(deserializedKey);
+        } else {
+            log.info("Adding/Updating by key='{}'", deserializedKey);
+            map.put(deserializedKey, deserializedValue);
+        }
+    }
+}
