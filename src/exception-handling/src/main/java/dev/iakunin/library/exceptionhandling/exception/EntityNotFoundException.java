8693f632799d92b7bbb60fa31e0792b5320b44cd
Maksim Iakunin <yakuninm+github@gmail.com>
2023-09-20 11:49:22 +0300

    :src:exception-handling Updating EntityNotFoundException

--- src/exception-handling/src/main/java/dev/iakunin/library/exceptionhandling/exception/EntityNotFoundException.java
+++ src/exception-handling/src/main/java/dev/iakunin/library/exceptionhandling/exception/EntityNotFoundException.java
@@ -2,28 +2,48 @@ package dev.iakunin.library.exceptionhandling.exception;
 
 import dev.iakunin.library.exceptionhandling.AbstractThrowableProblem;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.Value;
 import org.zalando.problem.Status;
 
 public class EntityNotFoundException extends AbstractThrowableProblem {
 
-    public EntityNotFoundException(String entityName, UUID id) {
-        this(entityName, id.toString());
+    public EntityNotFoundException(Class<?> entity, UUID id) {
+        this(entity, id.toString());
     }
 
-    public EntityNotFoundException(String entityName, String id) {
-        this(entityName, "id", id);
+    public EntityNotFoundException(Class<?> entity, String id) {
+        this(entity, "id", id);
     }
 
-    public EntityNotFoundException(String entityName, String fieldName, String fieldValue) {
+    public EntityNotFoundException(Class<?> entity, String fieldName, String fieldValue) {
+        this(entity, new Field(fieldName, fieldValue));
+    }
+
+    public EntityNotFoundException(Class<?> entity, Field... fields) {
         super(
             Status.NOT_FOUND,
             String.format(
-                "Unable to find '%s' entity by '%s'='%s'",
-                entityName,
-                fieldName,
-                fieldValue
+                "Unable to find entity '%s' by %s",
+                entity.getName(),
+                Stream.of(fields)
+                    .map(
+                        field -> String.format(
+                            "'%s'='%s'",
+                            field.getName(),
+                            field.getValue()
+                        )
+                    )
+                    .collect(Collectors.joining(" and "))
             )
         );
+    }
+
+    @Value
+    public static class Field {
+        String name;
+        String value;
     }
 
 }
